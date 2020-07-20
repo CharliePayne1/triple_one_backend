70ca5b9faf41de332fb8e72dda2532b2e7df7c1a
CharliePayne1 <charliepayne1101@gmail.com>
2020-07-20 16:44:11 +0100

    email automation complete

--- app/mailers/triple_one_email_mailer.rb
+++ app/mailers/triple_one_email_mailer.rb
@@ -2,9 +2,8 @@ class TripleOneEmailMailer < ApplicationMailer
     default from: Rails.application.secrets.email_user_name
 
     def decision_email(update_case)
-        byebug
         @update_case = update_case
-        mail(to: `#{update_case.email}`, subject: "A decision has been made about your case")
+        mail(to: "#{update_case.email}", subject: "A decision has been made about your case")
     end
     
 end
