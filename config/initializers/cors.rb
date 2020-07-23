7b3ad5a50553faaeb7889c06f86b152679ada421
CharliePayne1 <charliepayne1101@gmail.com>
2020-07-23 14:32:51 +0100

    pwa

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://triple-one-pwa.netlify.app/'
 
     resource '*',
       headers: :any,
